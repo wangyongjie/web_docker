783ceb5d2c94dc0168b94c7cf93d4fd20b0d4257
wangyongjie <shuimu.wang@kiwigame.com>
2021-10-11 11:04:39 +0800

    update

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-npm install -P && npm run build:prod && mv ./dist_docker ./dist_docker_copy && mv ./dist ./dist_docker && rm -rf ./dist_docker_copy
-
+npm install -P && npm run build:prod
+if [ -d './dist_docker' ]; then
+    mv ./dist_docker ./dist_docker_copy
+fi
+mv ./dist ./dist_docker
+if [ -d './dist_docker_copy' ]; then
+    rm -rf ./dist_docker_copy
+fi
