44ec6c1f717f564fd2278ef08648f3352631ad6c
wangyongjie <shuimu.wang@kiwigame.com>
2021-11-16 11:33:14 +0800

    admin

--- update.sh
+++ update.sh
@@ -10,15 +10,15 @@ if [ "$2" != '' ]; then
     branch=$2
 fi
 
-if [ "$project" == 'vue-admin-template' ] || [ "$project" == 'all' ]; then
-    echo '-------start update vue-admin-template-------'
+if [ "$project" == 'admin_frontend_template' ] || [ "$project" == 'all' ]; then
+    echo '-------start update admin_frontend_template-------'
     cd ../
-    if [ ! -d './vue-admin-template' ]; then
-        git clone -b $branch https://github.com/PanJiaChen/vue-admin-template.git
+    if [ ! -d './admin_frontend_template' ]; then
+        git clone -b $branch http://120.77.236.161/git/PublicGroup/admin_frontend_template.git
     fi
-    cp web_docker/build/docker-build.sh vue-admin-template/docker-build.sh
-    cp web_docker/build/build.sh vue-admin-template/build.sh
-    cd vue-admin-template
+    cp web_docker/build/docker-build.sh admin_frontend_template/docker-build.sh
+    cp web_docker/build/build.sh admin_frontend_template/build.sh
+    cd admin_frontend_template
     sh ./docker-build.sh
-    echo '-------end update vue-admin-template-------'
+    echo '-------end update admin_frontend_template-------'
 fi
